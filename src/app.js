26b875bd0dab1c2c51388e16e77af286253bebae
Deepak goyal <goyaldeepak871@gmail.com>
2025-07-30 18:25:10 +0530

    change11

--- src/app.js
+++ src/app.js
@@ -1,66 +1,69 @@
 // src/app.js
 const express = require('express');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const mongoSanitize = require('express-mongo-sanitize');
-const compression = require('compression');
-const cors = require('cors');
-const passport = require('passport');
-const httpStatus = require('http-status');
-const config = require('./config/config');
-const morgan = require('./config/morgan');
-const { jwtStrategy } = require('./config/passport');
-const { authLimiter } = require('./middlewares/rateLimiter');
-const routes = require('./routes/v1');
-const { errorConverter, errorHandler } = require('./middlewares/error');
-const ApiError = require('./utils/ApiError');
+// const helmet = require('helmet');
+// const xss = require('xss-clean');
+// const mongoSanitize = require('express-mongo-sanitize');
+// const compression = require('compression');
+// const cors = require('cors');
+// const passport = require('passport');
+// const httpStatus = require('http-status');
+// const config = require('./config/config');
+// const morgan = require('./config/morgan');
+// const { jwtStrategy } = require('./config/passport');
+// const { authLimiter } = require('./middlewares/rateLimiter');
+// const routes = require('./routes/v1');
+// const { errorConverter, errorHandler } = require('./middlewares/error');
+// const ApiError = require('./utils/ApiError');
 
 const app = express();
 
-// Logging
-if (config.env !== 'test') {
-  app.use(morgan.successHandler);
-  app.use(morgan.errorHandler);
-}
+// // Logging
+// if (config.env !== 'test') {
+//   app.use(morgan.successHandler);
+//   app.use(morgan.errorHandler);
+// }
 
-// Security
-app.use(helmet());
-app.use(xss());
-app.use(mongoSanitize());
+// // Security
+// app.use(helmet());
+// app.use(xss());
+// app.use(mongoSanitize());
 
-// JSON & URL encoding
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// // JSON & URL encoding
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-// Compression & CORS
-app.use(compression());
-app.use(cors());
-app.options('*', cors());
+// // Compression & CORS
+// app.use(compression());
+// app.use(cors());
+// app.options('*', cors());
 
-// JWT auth
-app.use(passport.initialize());
-passport.use('jwt', jwtStrategy);
+// // JWT auth
+// app.use(passport.initialize());
+// passport.use('jwt', jwtStrategy);
 
-// Rate limiter (optional)
-if (config.env === 'production') {
-  app.use('/v1/auth', authLimiter);
-}
+// // Rate limiter (optional)
+// if (config.env === 'production') {
+//   app.use('/v1/auth', authLimiter);
+// }
 
 // Routes
-app.use('/v1', routes);
+// app.use('/v1', routes);
 
 // 404
-app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
-});
+// app.use((req, res, next) => {
+//   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+// });
 
 // Custom response format
-app.response.sendJSONResponse = function ({ statusCode, status = true, message, data, isShowMessage = true }) {
-  return this.status(statusCode).json({ statusCode, status, message, isShowMessage, data });
-};
+// app.response.sendJSONResponse = function ({ statusCode, status = true, message, data, isShowMessage = true }) {
+//   return this.status(statusCode).json({ statusCode, status, message, isShowMessage, data });
+// };
 
-// Error handling
-app.use(errorConverter);
-app.use(errorHandler);
+// // Error handling
+// app.use(errorConverter);
+// app.use(errorHandler);
+app.get('/', (req, res) => {
+  res.send('API is working8888');
+});
 
 module.exports = app;
