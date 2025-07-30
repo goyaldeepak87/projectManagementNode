9e8385585f0929646f038fc1ae97d5dd24840a32
Deepak goyal <goyaldeepak871@gmail.com>
2025-07-31 01:56:50 +0530

    error19

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ const config = require('./config/config');
 const logger = require('./config/logger');
 
 let server;
-mongoose.connect('mongodb+srv://goyaldeepak871:8jKN5Bks6GLzuHAA@cluster0.mongodb.net/mydatabase?retryWrites=true&w=majority',  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then(() => {
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
   server = app.listen(config.port, () => {
     logger.info(`Listening to port ${config.port}`);
