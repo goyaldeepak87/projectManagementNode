742413a51e081782a0db89bb4b759a99ae47f5ea
Deepak goyal <goyaldeepak871@gmail.com>
2025-07-30 17:16:36 +0530

    changes1

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const serverless = require('serverless-http');
+const mongoose = require('mongoose');
+const app = require('../src/app');
+const config = require('../src/config/config');
+
+let isConnected;
+
+async function connectToDatabase() {
+  if (!isConnected) {
+    await mongoose.connect(config.mongoose.url, config.mongoose.options);
+    isConnected = true;
+    console.log("✅ MongoDB connected (vercel)");
+  }
+}
+
+module.exports = async (req, res) => {
+  await connectToDatabase();
+  return serverless(app)(req, res);
+};
--- src/index.js
+++ src/index.js
@@ -1,57 +1,40 @@
-// const mongoose = require('mongoose');
-// const app = require('./app');
-// const config = require('./config/config');
-// const logger = require('./config/logger');
+const mongoose = require('mongoose');
+const app = require('./app');
+const config = require('./config/config');
+const logger = require('./config/logger');
 
-// let server;
-// mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-//   logger.info('Connected to MongoDB');
-//   server = app.listen(config.port, () => {
-//     logger.info(`Listening to port ${config.port}`);
-//   });
-// }).catch((err)=>{
-//   console.log("xcxz",err)
-// })
+let server;
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+  logger.info('Connected to MongoDB');
+  server = app.listen(config.port, () => {
+    logger.info(`Listening to port ${config.port}`);
+  });
+}).catch((err)=>{
+  console.log("xcxz",err)
+})
 
-// const exitHandler = () => {
-//   if (server) {
-//     server.close(() => {
-//       logger.info('Server closed');
-//       process.exit(1);
-//     });
-//   } else {
-//     process.exit(1);
-//   }
-// };
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
 
-// const unexpectedErrorHandler = (error) => {
-//   logger.error(error);
-//   exitHandler();
-// };
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
 
-// process.on('uncaughtException', unexpectedErrorHandler);
-// process.on('unhandledRejection', unexpectedErrorHandler);
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
 
-// process.on('SIGTERM', () => {
-//   logger.info('SIGTERM received');
-//   if (server) {
-//     server.close();
-//   }
-// });
-
-
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('API is working');
-});
-
-app.listen(8000, () => {
-  console.log(`Server running on port 8000`);
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
 });
